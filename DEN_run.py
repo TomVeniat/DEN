ea8e73b88cc897a8ddf2d173f2e1ffb424c9b0d5
Tom <veniat.tom@gmail.com>
2020-11-15 20:20:50 +0100

    Load tasks

--- DEN_run.py
+++ DEN_run.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 import numpy as np
 import DEN
 from tensorflow.examples.tutorials.mnist import input_data
+import torch
+
 
 np.random.seed(1004)
 flags = tf.app.flags
@@ -10,7 +12,8 @@ flags.DEFINE_integer("max_iter", 4300, "Epoch to train")
 flags.DEFINE_float("lr", 0.001, "Learing rate(init) for train")
 flags.DEFINE_integer("batch_size", 256, "The size of batch for 1 iteration")
 flags.DEFINE_string("checkpoint_dir", "checkpoints", "Directory path to save the checkpoints")
-flags.DEFINE_integer("dims", [784, 312, 128, 10], "Dimensions about layers including output")
+# flags.DEFINE_integer("dims", [784, 312, 128, 10], "Dimensions about layers including output")
+flags.DEFINE_list("dims", [784, 312, 128, 10], "Dimensions about layers including output")
 flags.DEFINE_integer("n_classes", 10, 'The number of classes at each task')
 flags.DEFINE_float("l1_lambda", 0.00001, "Sparsity for L1")
 flags.DEFINE_float("l2_lambda", 0.0001, "L2 lambda")
@@ -19,29 +22,56 @@ flags.DEFINE_float("regular_lambda", 0.5, "regularization lambda")
 flags.DEFINE_integer("ex_k", 10, "The number of units increased in the expansion processing")
 flags.DEFINE_float('loss_thr', 0.01, "Threshold of dynamic expansion")
 flags.DEFINE_float('spl_thr', 0.05, "Threshold of split and duplication")
+
+flags.DEFINE_string('load_from', '2968', "Directory path to save the checkpoints")
 FLAGS = flags.FLAGS
 
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-trainX = mnist.train.images
-valX = mnist.validation.images
-testX = mnist.test.images
+if FLAGS.load_from is not None:
+	path = os.path.join('/data/veniat/lileb/datasets',
+						FLAGS.load_from,
+						'mnist-T{tid}_{split}.pth'
+																	)
+	trainXs, valXs, testXs = [], [], []
+	Xs = dict(
+		train=[],
+		val=[],
+		test=[],
+	)
+	for task in range(10):
+		for k, v in Xs.items():
+			loaded_x, loaded_y = torch.load(path.format(tid=task, split=k))
+			loaded_x = loaded_x.view(loaded_x.size(0), -1)
+			loaded_y = loaded_y
+			y = torch.zeros(loaded_x.size(0), 10)
+			y.scatter_(1, loaded_y, 1)
+			v.append((loaded_x.numpy(), y.numpy()))
+else:
+	mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+	trainX = mnist.train.images
+	valX = mnist.validation.images
+	testX = mnist.test.images
 
-task_permutation = []
-for task in range(10):
-	task_permutation.append(np.random.permutation(784))
+	task_permutation = []
+	for task in range(10):
+		task_permutation.append(np.random.permutation(784))
 
-trainXs, valXs, testXs = [], [], []
-for task in range(10):
-	trainXs.append(trainX[:, task_permutation[task]])
-	valXs.append(valX[:, task_permutation[task]])
-	testXs.append(testX[:, task_permutation[task]])
+	trainXs, valXs, testXs = [], [], []
+	for task in range(10):
+		trainXs.append(trainX[:, task_permutation[task]])
+		valXs.append(valX[:, task_permutation[task]])
+		testXs.append(testX[:, task_permutation[task]])
 
+
+# FLAGS.max_iter = 200
 model = DEN.DEN(FLAGS)
 params = dict()
 avg_perf = []
 
 for t in range(FLAGS.n_classes):
-	data = (trainXs[t], mnist.train.labels, valXs[t], mnist.validation.labels, testXs[t], mnist.test.labels)
+	if FLAGS.load_from is not None:
+		data = (*Xs['train'][t], *Xs['val'][t], *Xs['test'][t])
+	else:
+		data = (trainXs[t], mnist.train.labels, valXs[t], mnist.validation.labels, testXs[t], mnist.test.labels)
 	model.sess = tf.Session()
 	print("\n\n\tTASK %d TRAINING\n"%(t+1))
 
@@ -59,7 +89,11 @@ for t in range(FLAGS.n_classes):
 	model.load_params(params)
 	temp_perfs = []
 	for j in range(t+1):
-		temp_perf = model.predict_perform(j+1, testXs[j], mnist.test.labels)
+		if FLAGS.load_from is not None:
+			test_set = Xs['test'][t]
+		else:
+			test_set = (testXs[j], mnist.test.labels)
+		temp_perf = model.predict_perform(j+1, *test_set)
 		temp_perfs.append(temp_perf)
 	avg_perf.append( sum(temp_perfs) / float(t+1) )
 	print("   [*] avg_perf: %.4f"%avg_perf[t])
